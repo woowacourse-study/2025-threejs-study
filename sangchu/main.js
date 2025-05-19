dfc39b81abbac5c738a4d341871da9d07cb02249
sanghee01 <sang6299@naver.com>
2025-05-20 07:13:57 +0900

    week4 - 카메라 조작

--- sangchu/main.js
+++ sangchu/main.js
@@ -1,87 +1,140 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color("#87CEEB");
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x808080);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+// controls.enableZoom = false;
+// controls.screenSpacePanning = false;
+controls.minDistance = 1;
+controls.maxDistance = 10;
+// controls.maxPolarAngle = Math.PI / 2; // 수직 회전 제한
+// controls.autoRotate = true;
+// controls.autoRotateSpeed = 10;
+
+camera.position.set(0, 0, 1.5);
+camera.lookAt(0, 0, 0);
+
+const ambientLight = new THREE.AmbientLight("#ffffff");
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
+const rimLight = new THREE.DirectionalLight("#ffffff", 10);
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const basicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(boxGeometry, basicMaterial);
-cube.position.x = -2;
-scene.add(cube);
+rimLight.position.set(2, 0, -2);
 
-const sphereGeometry = new THREE.SphereGeometry(0.75, 32, 16);
-const standardMaterialRed = new THREE.MeshStandardMaterial({
-  color: 0xff0000,
-  metalness: 0.1,
+rimLight.target.position.set(0, 0, 0);
+
+scene.add(rimLight);
+scene.add(rimLight.target);
+
+const bottomLight = new THREE.DirectionalLight("#5183FF", 10);
+bottomLight.position.set(0, -2, 0);
+bottomLight.target.position.set(0, 0, 0);
+scene.add(bottomLight);
+scene.add(bottomLight.target);
+
+const keyLight = new THREE.DirectionalLight("#FFE062", 3);
+keyLight.position.set(-1, 1, 1);
+keyLight.target.position.set(0, 0, 0);
+scene.add(keyLight);
+scene.add(keyLight.target);
+
+let model;
+
+// addLightHelper(rimLight, bottomLight, keyLight);
+
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
+const planeMaterial = new THREE.MeshStandardMaterial({
+  color: "#84673b",
   roughness: 0.8,
+  metalness: 0.2,
 });
-const sphere = new THREE.Mesh(sphereGeometry, standardMaterialRed);
-sphere.position.x = 0;
-scene.add(sphere);
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.position.set(0, -1, 0);
+plane.rotation.x = -Math.PI / 2;
+plane.receiveShadow = true;
+scene.add(plane);
 
-const coneGeometry = new THREE.ConeGeometry(0.7, 1, 32);
-const standardMaterialBlue = new THREE.MeshStandardMaterial({
-  color: 0x0000ff,
-  metalness: 0.9,
-  roughness: 0.2,
-});
-const cone = new THREE.Mesh(coneGeometry, standardMaterialBlue);
-cone.position.x = 2;
-scene.add(cone);
+loadModel("/assets/sangchu-god.glb")
+  .then((loadedModel) => {
+    model = loadedModel;
+    model.scale.set(1, 1, 1);
+    model.position.set(0, 0, 0);
+    model.rotation.set(-0.5, -2.5, 0);
 
-const textureLoader = new THREE.TextureLoader();
+    scene.add(model);
+  })
+  .catch((error) => {
+    console.error("모델 로딩 실패:", error);
+  });
 
-textureLoader.load(
-  "./public/texture2.jpg",
-  function (texture) {
-    console.log("텍스처 로드 성공:", texture);
-
-    if (standardMaterialRed) {
-      standardMaterialRed.map = texture;
-      standardMaterialRed.needsUpdate = true;
-
-      console.log("Sphere에 텍스처 적용 완료!");
-    } else {
-      console.error(
-        "Sphere Material을 찾을 수 없습니다. 텍스처를 적용할 수 없습니다."
-      );
-    }
-  },
-  undefined,
-  function (err) {
-    console.error("텍스처 로드 중 오류 발생:", err);
-  }
-);
-
-camera.position.z = 5;
-
-function animate() {
+const animate = () => {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  sphere.rotation.x += 0.005;
-  sphere.rotation.y += 0.005;
-  cone.rotation.x += 0.005;
-  cone.rotation.y += 0.005;
+  if (model) {
+    model.rotation.y += 0.001;
+    model.rotation.x += 0.001;
+  }
+
+  controls.update();
 
   renderer.render(scene, camera);
+};
+animate();
+
+function loadModel(url) {
+  const loader = new GLTFLoader();
+  return new Promise((resolve, reject) => {
+    loader.load(
+      url,
+      (gltf) => resolve(gltf.scene),
+      undefined,
+      (error) => reject(error)
+    );
+  });
 }
 
-animate();
+function addLightHelper(rimLight, bottomLight, keyLight) {
+  if (rimLight) {
+    const rimLightHelper = new THREE.DirectionalLightHelper(
+      rimLight,
+      1,
+      "#ff0000"
+    );
+    scene.add(rimLightHelper);
+  }
+
+  if (bottomLight) {
+    const bottomLightHelper = new THREE.DirectionalLightHelper(
+      bottomLight,
+      1,
+      0x00ff00
+    );
+    scene.add(bottomLightHelper);
+  }
+
+  if (keyLight) {
+    const keyLightHelper = new THREE.DirectionalLightHelper(
+      keyLight,
+      1,
+      "#0000ff"
+    );
+    scene.add(keyLightHelper);
+  }
+}
