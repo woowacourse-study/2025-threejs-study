e7e2d50e70830c19592775969dda02d8294e56e9
Ko Daeun <drechsu@gmail.com>
2025-06-18 21:32:11 +0900

    refactor : PROFILE 정보 배열 consts 파일로 분리

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,32 +1,5 @@
 import Profile from '../components/Profile';
-
-const PROFILE = [
-  {
-    name: 'Blue',
-    imgUrl: 'https://avatars.githubusercontent.com/u/168459001?v=4',
-    linkUrl: 'https://github.com/hanheel/',
-  },
-  {
-    name: 'Sangchu',
-    imgUrl: 'https://avatars.githubusercontent.com/u/80993302?v=4',
-    linkUrl: 'https://github.com/sanghee01/',
-  },
-  {
-    name: 'Camel',
-    imgUrl: 'https://avatars.githubusercontent.com/u/141295691?v=4',
-    linkUrl: 'https://github.com/dev-dino22/',
-  },
-  {
-    name: 'Diane',
-    imgUrl: 'https://avatars.githubusercontent.com/u/141714293?v=4',
-    linkUrl: 'https://github.com/Daeun-100/',
-  },
-  {
-    name: 'Jenna',
-    imgUrl: 'https://avatars.githubusercontent.com/u/106021313?v=4',
-    linkUrl: 'https://github.com/JeLee-river/',
-  },
-];
+import { PROFILE } from '../consts';
 
 const Test = () => {
   return (
