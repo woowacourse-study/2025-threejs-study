57250a1ff1d18163ecfdf15693905e0fd6b0c5ba
hanheel <hanhee630630@gmail.com>
2025-06-18 18:43:10 +0900

    refactor: 테스트 컴포넌트 Test.tsx로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/routes";
-import SceneCard from "./components/sceneCard/SceneCard";
 
 const App = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-      <SceneCard imageSrc="/scene-1.png" name="Scene 1" />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,8 +1,11 @@
+import SceneCard from "../components/sceneCard/SceneCard";
+
 const Test = () => {
   return (
     <div>
       <h1>Test Component</h1>
       <p>This is a test component to verify the setup.</p>
+      <SceneCard imageSrc="/scene-1.png" name="Scene 1" />
     </div>
   );
 };
