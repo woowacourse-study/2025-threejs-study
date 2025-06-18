f3492ae735ede7a72e15d231f984dd5075507655
hanheel <hanhee630630@gmail.com>
2025-06-18 17:57:40 +0900

    refactor: theme 중 사용하지 않는 color 삭제

--- src/theme.ts
+++ src/theme.ts
@@ -1,26 +1,25 @@
-import type { CSSObject } from '@emotion/react';
+import type { CSSObject } from "@emotion/react";
 
 export const theme = {
   colors: {
-    white: '#FFFFFF',
-    gray1: '#F1F3F5',
-    gray2: '#ADB5BD',
-    gray3: '#212529',
+    white: "#FFFFFF",
+    gray: "#ADB5BD",
+    black: "#212529",
   },
   fonts: {
     header: {
-      fontSize: '32px',
-      lineHeight: '44px',
+      fontSize: "32px",
+      lineHeight: "44px",
       fontWeight: 700,
     } as CSSObject,
     subHeading: {
-      fontSize: '16px',
-      lineHeight: '19px',
+      fontSize: "16px",
+      lineHeight: "19px",
       fontWeight: 500,
     } as CSSObject,
     body: {
-      fontSize: '12px',
-      lineHeight: 'normal',
+      fontSize: "12px",
+      lineHeight: "normal",
       fontWeight: 400,
     } as CSSObject,
   },
