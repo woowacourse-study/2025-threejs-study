866da8f6f1546c80661d214f4a6eb89bdf8dbe4d
Jeongeun Lee <leeju1009@gmail.com>
2025-06-18 17:28:56 +0900

    feat: Banner 컴포넌트 생성

---
+++ src/components/Banner.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+const Banner = () => {
+  return (
+    <StyledBanner>
+      <SubTitle>woowahan-three-js products.</SubTitle>
+    </StyledBanner>
+  );
+};
+
+export default Banner;
+
+const StyledBanner = styled.section`
+  width: 100%;
+  padding: 194px 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SubTitle = styled.h2(({ theme }) => ({
+  width: '332px',
+
+  textAlign: 'center',
+  whiteSpace: 'wrap',
+  ...theme.fonts.header,
+}));
