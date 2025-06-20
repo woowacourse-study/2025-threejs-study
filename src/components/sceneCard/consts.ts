06ddea1cfe240035fd5b72fe9c8cfc6678887eca
dev-dino22 <wlsskdud02@naver.com>
2025-06-20 16:47:42 +0900

    feat: SceneCard의 데이터 상수파일 제작

---
+++ src/components/sceneCard/consts.ts
@@ -0,0 +1,39 @@
+type SCENE_CARDS = {
+  id: string;
+  name: string;
+  imageSrc: string;
+  sceneUrl: string;
+};
+
+export const SCENE_CARDS: SCENE_CARDS[] = [
+  {
+    id: "hanheel",
+    name: "Blue",
+    imageSrc: "https://avatars.githubusercontent.com/u/168459001?v=4",
+    sceneUrl: "https://github.com/hanheel/",
+  },
+  {
+    id: "sanghee01",
+    name: "Sangchu",
+    imageSrc: "https://avatars.githubusercontent.com/u/80993302?v=4",
+    sceneUrl: "https://github.com/sanghee01/",
+  },
+  {
+    id: "dev-dino22",
+    name: "Camel",
+    imageSrc: "https://avatars.githubusercontent.com/u/141295691?v=4",
+    sceneUrl: "https://github.com/dev-dino22/",
+  },
+  {
+    id: "Daeun-100",
+    name: "Diane",
+    imageSrc: "https://avatars.githubusercontent.com/u/141714293?v=4",
+    sceneUrl: "https://github.com/Daeun-100/",
+  },
+  {
+    id: "JeLee-river",
+    name: "Jenna",
+    imageSrc: "https://avatars.githubusercontent.com/u/106021313?v=4",
+    sceneUrl: "https://github.com/JeLee-river/",
+  },
+];
