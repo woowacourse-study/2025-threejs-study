5118a78314d5fa8a9c582a5ad1ae2fb4fb7b1be7
hanheel <hanhee630630@gmail.com>
2025-06-27 16:53:52 +0900

    refactor: consts 폴더 위치 수정

---
+++ src/pages/consts.ts
@@ -0,0 +1,41 @@
+import { ROUTES } from '../constants/routes';
+
+type SceneCards = {
+  id: string;
+  name: string;
+  imageSrc: string;
+  sceneUrl: string;
+};
+
+export const SCENE_CARDS: SceneCards[] = [
+  {
+    id: 'hanheel',
+    name: 'bass',
+    imageSrc: '/thumbnail/bass.png',
+    sceneUrl: ROUTES.SCENES.BASS,
+  },
+  {
+    id: 'sanghee01',
+    name: 'Sangchu',
+    imageSrc: 'https://avatars.githubusercontent.com/u/80993302?v=4',
+    sceneUrl: 'https://github.com/sanghee01/',
+  },
+  {
+    id: 'dev-dino22',
+    name: 'Camel',
+    imageSrc: 'https://avatars.githubusercontent.com/u/141295691?v=4',
+    sceneUrl: 'https://github.com/dev-dino22/',
+  },
+  {
+    id: 'Daeun-100',
+    name: 'card orbit',
+    imageSrc: '/thumbnail/card-orbit.png',
+    sceneUrl: ROUTES.SCENES.CARD_ORBIT,
+  },
+  {
+    id: 'JeLee-river',
+    name: 'Jenna',
+    imageSrc: 'https://avatars.githubusercontent.com/u/106021313?v=4',
+    sceneUrl: 'https://github.com/JeLee-river/',
+  },
+];
