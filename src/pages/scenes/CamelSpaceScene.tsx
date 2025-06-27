2a859e3e4302fc6cfd09ba5b2f92d783777fcc4d
dev-dino22 <wlsskdud02@naver.com>
2025-06-28 00:32:20 +0900

    style: 컨테이너에 scroll hidden 처리

--- src/pages/scenes/CamelSpaceScene.tsx
+++ src/pages/scenes/CamelSpaceScene.tsx
@@ -12,7 +12,9 @@ const CamelSpaceScene = () => {
 
 const CamelSpaceSceneContainer = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  box-sizing: border-box;
+  overflow: hidden;
 `;
 
 export default CamelSpaceScene;
