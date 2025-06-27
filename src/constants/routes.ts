005c7722789bea41e064af90ca7d20dbdf56df1a
hanheel <hanhee630630@gmail.com>
2025-06-27 16:48:05 +0900

    feat: cardOrbit 씬 삽입

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,5 +2,6 @@ export const ROUTES = {
   HOME: '/',
   SCENES: {
     BASS: '/scenes/bass',
+    CARD_ORBIT: '/scenes/card-orbit',
   },
 };
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { ROUTES } from '../constants/routes';
 import Main from '../pages/Main';
 import BassScene from '../pages/scenes/BassScene';
+import CardOrbitScene from '../pages/scenes/CardOrbitScene';
 
 export const router = createBrowserRouter([
   {
@@ -11,5 +12,9 @@ export const router = createBrowserRouter([
   {
     path: ROUTES.SCENES.BASS,
     element: <BassScene />,
+  },
+  {
+    path: ROUTES.SCENES.CARD_ORBIT,
+    element: <CardOrbitScene />,
   },
 ]);
