9dfacbfa6a5f845c80438e8f45acbbe7b8bc845e
hanheel <hanhee630630@gmail.com>
2025-06-29 11:23:08 +0900

    chore: resolve conflict

--- src/pages/scenes/const.ts
+++ src/pages/scenes/const.ts
@@ -1,5 +1,5 @@
 export const SCENE_URLS = {
-  blue: '/scenes/bass/index.html',
+  bass: '/scenes/bass/index.html',
   camelSpace: '/scenes/camelSpace/index.html',
-  diane: '/scenes/cardOrbit/index.html',
+  cardOrbit: '/scenes/cardOrbit/index.html',
 };
