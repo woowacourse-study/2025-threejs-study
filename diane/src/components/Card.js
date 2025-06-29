5365ade614b28cf0746f8af233dad73f829cda90
Ko Daeun <drechsu@gmail.com>
2025-06-29 14:36:02 +0900

    refactor : card.glb를 asset으로 이동

--- diane/src/components/Card.js
+++ diane/src/components/Card.js
@@ -1,10 +1,11 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import CardUrl from '../assets/card.glb';
 
 export const loadCardModel = async () => {
   const loader = new GLTFLoader();
   return new Promise((resolve, reject) => {
     loader.load(
-      '/card.glb', // public 폴더 기준 경로
+      CardUrl, // public 폴더 기준 경로
       (gltf) => {
         const model = gltf.scene;
         model.scale.set(0.7, 0.7, 0.7); // 크기 조절 (필요 시)
